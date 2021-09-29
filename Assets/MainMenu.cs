938aa5a44f955e8097f0d00fd470843f7013db32
Fabrrr <91399044+Fabrrr@users.noreply.github.com>
2021-09-28 23:22:13 -0300

    Add Options

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -2,10 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
 
 public class MainMenu : MonoBehaviour
 {
-
+public AudioMixer audioMixer;
+public void SetFullscreen (bool isFullScreen)
+{
+    Screen.fullScreen = isFullScreen;
+}
+public void SetVolume (float volume)
+{
+    audioMixer.SetFloat("Volumen",  volume);
+}
 public void PlayGame ()
 {
     SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
