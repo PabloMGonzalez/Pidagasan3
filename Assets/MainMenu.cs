fc1a57468cc0aa1c89b793991a605eec40b95e16
Fabrrr <91399044+Fabrrr@users.noreply.github.com>
2021-09-29 12:06:49 -0300

    Add Options
    No "funciona" el mostrar las resoluciones.
    Cambie el "Dropdown" por "TMP_Dropdown" para poder usar el objeto Text Mesh Pro Dropdown. Quizas sea eso?

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Audio;
-
+using UnityEngine.UI;
+using TMPro;
 
 public class MainMenu : MonoBehaviour
 {
+    public TMP_Dropdown ResolutionDropdown;
+ Resolution[] resolutions;
+        private void Start()
+    {
+        int CurrentResolutionIndex = 0;
+        resolutions = Screen.resolutions;
+
+        ResolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            string Option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(Option);
+
+            if(resolutions[i].width == Screen.width &&
+                resolutions[i].height == Screen.height)
+            {
+                CurrentResolutionIndex = i;
+            }
+        }
+
+        ResolutionDropdown.AddOptions(options);
+        ResolutionDropdown.value = CurrentResolutionIndex;
+        ResolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolution(int ResolutionIndex)
+    {
+        Resolution resolution = resolutions[ResolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
 public AudioMixer audioMixer;
 public void SetFullscreen (bool isFullScreen)
 {
