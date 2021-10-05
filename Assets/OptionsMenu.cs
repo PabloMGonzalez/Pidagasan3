e7342b4db1383b9cb00e14e30f7333869e6c7738
Fabrrr <91399044+Fabrrr@users.noreply.github.com>
2021-10-05 17:06:11 -0300

    Add Resolution
    me cago en esta mierda

--- Assets/OptionsMenu.cs
+++ Assets/OptionsMenu.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-
 public class OptionsMenu : MonoBehaviour
 {
 public void SetFullscreen (bool isFullScreen)
 {
     Screen.fullScreen = isFullScreen;
+}
+
+List<int> widths = new List<int>() {568, 960, 1280, 1920};
+List<int> eights = new List<int>() {320, 540, 800, 1080};
+
+public void SetScreenSize (int index)
+{
+    bool fullscreen = Screen.fullScreen;
+    int width = widths[index];
+    int eight = eights[index];
+    Screen.SetResolution(width, eight, fullscreen);
 }
 
 }
